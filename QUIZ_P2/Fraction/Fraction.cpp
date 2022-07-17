37c895c6992dc29f704ca8126aa91cf52cebe45d
EddyG007 <109482576+EddyG007@users.noreply.github.com>
2022-07-17 21:50:21 +0200

    Dans Fraction.cpp
    Bonsoir chères formateurs… Bon j'connais pas grand chose sur GitHub, je viens à peine de créer mon compte 
    Mais bon, je suis actuellement le cours sur la POO avec C++ et j'aimerais bien (si possible) proposé un ajout a la fonction membre : 
    void Fraction::affiche(std::ostream& flux) const. (Si possible évidemment)
    Merci de bien recevoir ma proposition.

--- QUIZ_P2/Fraction/Fraction.cpp
+++ QUIZ_P2/Fraction/Fraction.cpp
@@ -16,6 +16,11 @@ Fraction::Fraction()
 
 void Fraction::affiche(std::ostream& flux) const
 {
+     if(m_denominateur == 0)
+    {
+        flux << "Le denominateur est egal a 0 donc Erreur" << endl;
+        exit(0);
+    }
     if(m_denominateur == 1)
     {
         flux << m_numerateur;
